a84aad600ad2e381316c8a781a50cc50be3b67f1
Rubén Gavilán <rgavilan@izertis.com>
2023-11-15 11:00:09 +0100

    first commit

---
+++ src/main/java/com/izertis/codacypoc/CodacyPocApplication.java
@@ -0,0 +1,13 @@
+package com.izertis.codacypoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodacyPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodacyPocApplication.class, args);
+	}
+
+}
